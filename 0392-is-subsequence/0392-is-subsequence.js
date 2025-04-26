5e96b6dca2cfa99af2cacb4b92b3980c852408b1
Hari-0210 <103021640+Hari-0210@users.noreply.github.com>
2025-04-26 12:54:58 +0530

    Time: 1 ms (73.2%), Space: 54.4 MB (83.4%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let sIndex = 0,
+    tIndex = 0;
+
+  if (s === "") {
+    return true;
+  }
+
+  while (tIndex < t.length) {
+    if (s[sIndex] === t[tIndex]) {
+      sIndex++;
+    }
+    tIndex++;
+    if (sIndex === s.length) {
+      return true;
+    }
+  }
+
+  return false;
+};
