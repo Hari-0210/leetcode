c4cbead41e7fad9abd91b917a70b21288c0f8339
Hari-0210 <103021640+Hari-0210@users.noreply.github.com>
2025-05-15 23:41:45 +0530

    Time: 9 ms (39.69%), Space: 59.9 MB (46.41%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if (numRows === 1 || s.length <= numRows) {
+    return s;
+  }
+
+  const rows = Array.from({ length: Math.min(numRows, s.length) }, () => "");
+  let curRow = 0;
+  let goingDown = false;
+
+  for (let char of s) {
+    rows[curRow] += char;
+    if (curRow === 0 || curRow === numRows - 1) {
+      goingDown = !goingDown;
+    }
+    curRow += goingDown ? 1 : -1;
+  }
+
+  return rows.join("");
+};
