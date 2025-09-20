d25cd56476fd41d637cbc03878466965dc790430
Hari-0210 <103021640+Hari-0210@users.noreply.github.com>
2025-09-20 22:29:34 +0530

    Time: 33 ms (33.58%), Space: 55 MB (55.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,11 +4,9 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  let ans = 0;
-  for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
-      ans = nums[i] + nums[j];
-      if (ans === target) {
+      if (nums[i] + nums[j] === target) {
         return [i, j];
       }
     }
